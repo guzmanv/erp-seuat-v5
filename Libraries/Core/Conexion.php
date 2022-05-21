7ad76bfb7f5ad9161b43c4ba00c84889a6d1c71f
josesantiz-dev <JOSE_SANTIZR1990@HOTMAIL.COM>
2022-05-21 12:38:21 -0500

    configuracion de archivos de conexion

--- Libraries/Core/Conexion.php
+++ Libraries/Core/Conexion.php
@@ -1,33 +1,22 @@
 <?php
 class Conexion{
-	//private $conect;
+	private $conect;
+
 	public function __construct(){
-        /* foreach (conexiones as $key => $conexion) {
-            try{
-                $this->conect[$key] = new PDO("mysql:host=".$conexion['DB_HOST'].";dbname=".$conexion['DB_NAME'].";charset=".$conexion['DB_CHARSET'],$conexion['DB_USER'],$conexion['DB_PASSWORD']);
-                $this->conect[$key]->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            }catch (PDOException $e){
-                $this->conect[$key] = 'Error de conexión';
-                echo "ERROR: " . $e->getMessage();
-  
-            }
-        } */
+		$connectionString = "mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=".DB_CHARSET;
+		try{
+			$this->conect = new PDO($connectionString, DB_USER, DB_PASSWORD);
+			$this->conect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		    //echo "conexión exitosa";
+		}catch(PDOException $e){
+			$this->conect = 'Error de conexión';
+		    echo "ERROR: " . $e->getMessage();
+		}
 	}
 
-	/* public function conect($bd){
-		return $this->conect[$bd];
-	} */
-    public function conect($bd){
-        try{
-            $c = new PDO("mysql:host=".conexiones[$bd]['DB_HOST'].";dbname=".conexiones[$bd]['DB_NAME'].";charset=".conexiones[$bd]['DB_CHARSET'],conexiones[$bd]['DB_USER'],conexiones[$bd]['DB_PASSWORD']);
-            $c->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            return $c;
-        }catch(PDOException $e){
-            $msg = 'Error de conexión';
-            return $msg;
-            echo "ERROR: " . $e->getMessage();
-        }
-    }
+	public function conect(){
+		return $this->conect;
+	}
 }
 
 ?>
