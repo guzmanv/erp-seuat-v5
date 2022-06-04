28e97e94cd11c6ab1f781f4d7946b1927d3a9f93
josesantiz-dev <JOSE_SANTIZR1990@HOTMAIL.COM>
2022-06-04 09:12:58 -0500

    se quito seccion exportar prospecto

--- Views/Template/nav_admin.php
+++ Views/Template/nav_admin.php
@@ -313,14 +313,14 @@
                                 </p>
                             </a>
                         </li>
-                        <li class="nav-item">
+                        <!-- <li class="nav-item">
                             <a href="<?php echo BASE_URL ?>/ExportarProspectos/" class="nav-link">
                                 <i class="ml-3 mr-2" data-feather="arrow-down"></i>
                                 <p>
                                     Exportar prospectos
                                 </p>
                             </a>
-                        </li>
+                        </li> -->
                         <!-- <li class="nav-item">
                             <a href="<?php echo BASE_URL ?>/ImportarProspectos/" class="nav-link">
                                 <i class="ml-3 mr-2" data-feather="arrow-up"></i>
