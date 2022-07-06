f7313b0221b65ce002a576437ea71f22e5ac4dc9
josesantiz-dev <JOSE_SANTIZR1990@HOTMAIL.COM>
2022-07-06 16:06:20 -0500

    inscripcion

---
+++ Controllers/Inscripcion.php
@@ -0,0 +1,30 @@
+<?php
+    class Inscripcion extends Controllers{
+        private $idUser;
+		private $rol;
+        private $idPlantel;
+		public function __construct()
+		{
+			parent::__construct();
+			session_start();
+		    if(empty($_SESSION['login']))
+		    {
+			    header('Location: '.base_url().'/login');
+			    die();
+		    }
+			$this->idUser = $_SESSION['idUser'];
+			$this->rol = 'aux';
+            $this->idPlantel = 1;
+		}
+        //Mostrar vista de ingresos
+        public function inscripcion(){
+            $data['page_id'] = 10;
+            $data['page_tag'] = "Inscripcion";
+            $data['page_title'] = "Inscripcion";
+            $data['page_content'] = "";
+            $data['page_functions_js'] = "functions_inscripcion.js";
+            $this->views->getView($this,"inscripcion",$data);
+            
+        }
+    }
+?>
---
+++ Views/Inscripcion/inscripcion.php
@@ -0,0 +1,56 @@
+<?php
+    headerAdmin($data);
+    /* getModal('Instituciones/modalNuevaInstitucion',$data); */
+?>
+<div id="contentAjax"></div>
+<div class="wrapper">
+    <div class="content-wrapper">
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-7">
+                        <h1 class="m-0">  <?= $data['page_title'] ?></h1>
+                    </div>
+                    <div class="col-sm-5">
+                        <ol class="breadcrumb float-sm-right btn-block">
+                            <button type="button"  class="btn btn-inline btn-primary btn-sm btn-block" data-toggle="modal"  onclick="btnNuevaInscripcion()" data-target="#modal_form_nueva_inscripcion"><i class="fa fa-plus-circle fa-md"></i> Nuevo</button>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="card">
+                            <div class="card-body">
+                               <p class="card-text">
+                                    <table id="tableInscripciones" class="table table-bordered table-striped table-hover table-sm">
+                                        <thead>
+                                            <tr>
+                                                <th>#</th>
+                                                <th>Plantel</th>
+                                                <th>Institucion</th>
+                                                <th>Grado</th>
+                                                <th>Grupo</th>
+                                                <th>Alumnos</th>
+                                                <th>Acciones</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+
+                                        </tbody>
+                                    </table>
+                                </p>
+                            </div>
+                        </div>  
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php footerAdmin($data); ?>
+
+
